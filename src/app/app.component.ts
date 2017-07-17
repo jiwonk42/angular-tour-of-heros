883f4740d2ad634a6d077ac09c0bbbd4e6afca95
Jiwon Kang <jiwonk42@gmail.com>
2017-07-17 15:57:49 -0700

    move Hero array to a mock hero data, create a hero service, and inject the service into app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Hero } from './hero.model';
+import { HeroService } from './hero.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [HeroService]
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+  constructor(private heroService: HeroService) {}
+
   title: string = 'Tour of Heros';
+  heroes: Hero[];
   selectedHero: Hero;
-  heroes: Hero[] = [
+
+  /*heroes: Hero[] = [
     new Hero(1, 'Windstorm'),
     new Hero(2, 'Spiderman'),
     new Hero(3, 'Superman')
-  ];
+  ];*/
+
+  ngOnInit() {
+    this.heroes = this.heroService.getHeroes();
+  }
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,7 @@
+import { Hero } from './hero.model';
+
+export const HEROES: Hero[] = [
+  new Hero(1, 'Windstorm'),
+  new Hero(2, 'Spiderman'),
+  new Hero(3, 'Superman')
+];
